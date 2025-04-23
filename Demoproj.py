930aaa531c4223a1d62cfe02d84008516d761453
Rahul <nayakrahul085@gmail.com>
2025-04-23 12:16:03 +0530

    done encapsulation

--- Demoproj.py
+++ Demoproj.py
@@ -1,11 +1,30 @@
 
 class chatbook:
+
+    __user_id = 0
     def __init__(self):
+        self.__name="Default User"
+        
+        self.id= chatbook.__user_id
+        chatbook.__user_id += 1
         self.username=""
         self.password=""
         self.loggedin=False
-        self.menu()
-        
+        #self.menu()
+    
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id=val
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self,value):
+        self.__name=value
 
     def menu(self):
         user_input=input("""Welcome to chatbook , how would u like to proceed?
--- module.py
+++ module.py
@@ -1,4 +1,19 @@
 from Demoproj import chatbook
 
 user2 =chatbook()
+print(user2.id)
 
+chatbook.set_id(10)
+print(chatbook.get_id())
+#print(user2._chatbook__name)
+#user2._chatbook__menu()
+
+"""print(user2.get_name())
+user2.set_name("Rahul")
+print(user2.get_name())"""
+"""
+user3 =chatbook()
+print(user3.id)
+
+user4 =chatbook()
+print(user4.id)"""
